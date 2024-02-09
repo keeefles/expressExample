e2e7a43246dcd40f52380ffe7364137274b27e08
Aakeefah Jones <aakeefahj@gmail.com>
2024-02-09 16:17:32 +0200

    express done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Welcome to Express JS");
+import express from 'express'
+// const express = require('express')
+
+/* HTTP Methods: 
+get: retrieve data
+post: submitting the data
+put: puts in a new request to change the data?
+patch: is faster than put
+delete:
+*/
+
+// create an express app then set up a router.
+const app = express()
+const port = ++process.env.PORT || 4000
+
+// routers
+app.get('/', (req, res) =>{
+    res.json({
+        status: res.statusCode,
+        msg: "You're home"
+    })
+})
+app.get('/', (req, res) =>{
+    res.json({
+        status: res.statusCode,
+        msg: "About Page"
+    })
+})
+app.all('*', (req, res) =>{
+    res.json({
+        status: 404,
+        msg: "Error 404"
+    })
+})
+
+app.listen(port)
